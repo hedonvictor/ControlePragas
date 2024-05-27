f36b9e34064017c556d20f72d317de38a8969999
NathanRibeiro14 <nathanribeiropaula@gmail.com>
2024-05-27 19:41:02 -0300

    pestControl com a tela completa

--- lib/pages/components/customTextButton.dart
+++ lib/pages/components/customTextButton.dart
@@ -12,9 +12,6 @@ class CustomTextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(text),
-    );
+    return ElevatedButton(onPressed: onPressed, child: Text(text));
   }
 }
